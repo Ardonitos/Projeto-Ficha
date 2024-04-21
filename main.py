37ae6a4f3851632da19542cde1d260ea43d91fde
Rafael Silveira <144189737+DevR-Silveira@users.noreply.github.com>
2024-04-21 11:22:07 -0300

    Register Update
    - Implementada a pagina de registro, com a função de inserir os dados no banco de dados;
    - também implementado melhorias tanto no gerenciador do banco de dados, tanto no main de modo a converter os inserts para o padrão do banco de dados;
    - e por fim há o pequeno esboço da pagina de consulta

--- main.py
+++ main.py
@@ -5,10 +5,27 @@ além de também conectar ao banco de dados PostgreSQL
 
 from os import getenv
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, jsonify, request, render_template
 from pg_manager import db_connect, CrudOperations
 
+def float_conversor(value: list) -> list:
+    list_index = [5, 6, 7]
+    for i in list_index:
+        if value[i] is not None:
+            value[i] = float(value[i])
+    return value
 
+def date_conversor(value: list[tuple]) -> list[list]:
+    date_index = 4
+    return_value = []
+    for i in value:
+        return_value.append(list(i))
+    
+    for i in return_value:
+        i[date_index] = str(i[date_index])
+
+    return return_value
+        
 
 
 
@@ -18,14 +35,34 @@ if __name__ == '__main__':
     DBNAME, DBUSER, DBPASSWORD = getenv('DBNAME'), getenv('DBUSER'), getenv('DBPASSWORD')
 
     conn, cur = db_connect(DBNAME,DBUSER,DBPASSWORD)
-
     operations = CrudOperations(conn, cur)
+
     
-    ...
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return render_template('site.html')
+    
+    @app.route('/consulta')
+    def consulta():
+        return render_template('consulta.html')
+
+    @app.route('/read', methods=['GET'])
+    def read_db():
+        rows = operations.read_all_data()
+        print(date_conversor(rows))
+        return jsonify(date_conversor(rows))
+
+    @app.route('/insert', methods=['POST'])
+    def insert_into_db():
+        data_request: dict = request.get_json()
+        data = list(data_request.values())
+        data.insert(0, operations.max_id()+1)
+        converted_data = float_conversor(data)
+        print(converted_data)
+        operations.insert_data(converted_data)
+        return jsonify('Ok!')
 
 
-
-
-
-
-
+    app.run(debug=True)
--- pg_manager.py
+++ pg_manager.py
@@ -2,9 +2,9 @@
 Pequeno Gerenciador para pequenas operações CRUD no PostgreSQL
 """
 
-
 import psycopg2
 from psycopg2.extensions import connection, cursor
+from psycopg2.sql import SQL, Identifier
 
 def db_connect(dbname:str, user:str, password:str):
     'Conect to the database and return the connection and cursor objects'
@@ -36,12 +36,26 @@ class CrudOperations:
         except Exception as e:
             print('Insert Fail')
             print(e.__class__.__name__, e)
+            self.conn.rollback()
     
-    def read_data(self):
-        'get all the information in the table'
-        self.cur.execute("""SELECT * FROM record_table""")
+    def read_all_data(self) -> list[tuple]:
+        'get all the information stored in the table'
+        self.cur.execute(SQL("""SELECT * FROM record_table ORDER BY id ASC"""))
         rows = self.cur.fetchall()
         return rows
+
+    def read_data(self, field):
+        'get some especific information in the table'
+        self.cur.execute(SQL("""SELECT {} FROM record_table ORDER BY id ASC""").format(Identifier(field)))
+        rows = self.cur.fetchall()
+        return rows
+    
+    def max_id(self):
+        'get the max id number'
+        self.cur.execute("SELECT MAX(id) FROM record_table")
+        max_id = self.cur.fetchone()
+        return max_id[0]
+        
 
     def update_data(self):
         ...
